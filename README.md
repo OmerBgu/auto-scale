52670a4113f6cb587de4aa01f872706de03dcea0
Your Name <you@example.com>
2017-07-24 03:07:43 +0300

    add client.py that run client

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # auto-scale
 auto scale for kubernetes container
+use by ./run_main.sh
+rf.py- prediction of random forest based o ntran and test csv files
+main.py- make desicion on sacle or not to scale 
---
+++ client.py
@@ -0,0 +1,40 @@
+import subprocess
+import time
+import numpy as np
+import webbrowser
+import requests
+import csv
+import datetime
+import sys
+
+
+
+if __name__ == "__main__":
+    #this script take as a parameter the pods number
+    url='http://a8826b8f8422b11e791bf061beb5b7cb-726656503.us-west-2.elb.amazonaws.com/'
+    #yakurl='http://a8826b8f8422b11e791bf061beb5b7cb-726656503.us-west-2.elb.amazonaws.com/'
+
+
+    for x in range(0, 100):
+        #print ("x= %d"%(x))
+        response=requests.get(url)
+        #response2 = requests.get(urlscl)
+        #response3 = requests.get(yakurl)
+        with open('DataDelay.csv', 'a', newline='') as csvfile:
+            fieldnames = ['delay time', 'date','pods']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            delay=response.elapsed.total_seconds()
+            #date=response.headers['Date']
+            date = datetime.datetime.now()
+            writer.writerow({'delay time': delay, 'date': date,'pods':sys.argv[1]})
+
+
+        #incognito mode to enter and watch the entire movie
+        if x < 10:
+            subprocess.Popen(["C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "-incognito", url])
+        t = np.random.exponential(2)
+
+
+
+
+
